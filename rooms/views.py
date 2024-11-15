71a9fb14ae0819b979b99b3bd18dd803acb16c08
juaruiwar <juaruiwar@alum.us.es>
2024-11-15 17:32:35 +0100

    feat: community areas model implemented

--- rooms/views.py
+++ rooms/views.py
@@ -3,8 +3,8 @@ from .models import Sala
 
 def lista_salas(request):
     salas = Sala.objects.all()
-    return render(request, 'salas/lista_salas.html', {'salas': salas})
+    return render(request, 'rooms/lista_salas.html', {'salas': salas})
 
 def reserva_sala(request, sala_id):
     sala = Sala.objects.get(id=sala_id)
-    return render(request, 'salas/reserva_sala.html', {'sala': sala})
+    return render(request, 'rooms/reserva_sala.html', {'sala': sala})
