a920633ac39c59a42b7a747d648e78b77c2b6a21
juaruiwar <juaruiwar@alum.us.es>
2024-11-15 17:32:35 +0100

    lista de rooms

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Sala
 
-# Register your models here.
+@admin.register(Sala)
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+    search_fields = ('nombre',)
