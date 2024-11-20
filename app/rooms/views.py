2df1bb22a7e141c25c76a30c914d2d95431c19d9
juaruiwar <juaruiwar@alum.us.es>
2024-11-20 18:32:38 +0100

    Functional buttons to review booking

--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Sala
+from .models import Valoracion
 
 
 def lista_salas(request):
@@ -10,3 +11,11 @@ def lista_salas(request):
 def reserva_sala(request, sala_id):
     sala = Sala.objects.get(id=sala_id)
     return render(request, "rooms/reserva_sala.html", {"sala": sala})
+
+def valorar_experiencia(request):
+    if request.method == 'POST':
+        satisfecho = request.POST.get('valoracion') == 'positiva'
+        Valoracion.objects.create(satisfecho=satisfecho)
+        return redirect('lista_salas')
+    
+    return render(request, 'rooms/valoracion.html')
