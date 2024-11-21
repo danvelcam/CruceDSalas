2f097f97cc2d34eedeab9626925d46aef7a93b70
juaruiwar <juaruiwar@alum.us.es>
2024-11-21 13:14:27 +0100

    feat: booking done, error when uploading

--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
-from .models import Sala
-from .models import Valoracion
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Sala, Reserva, Valoracion
+from .forms import ReservaForm
+from django.utils import timezone
+from datetime import timedelta
 
 def lista_salas(request):
     salas = Sala.objects.all()
@@ -19,3 +21,27 @@ def valorar_experiencia(request):
         return redirect('lista_salas')
     
     return render(request, 'rooms/valoracion.html')
+
+@login_required
+def reserva_sala(request, sala_id):
+    sala = get_object_or_404(Sala, id=sala_id)
+    fecha_actual = timezone.now().date()
+    fecha_limite = fecha_actual + timedelta(days=7)
+
+    if request.method == 'POST':
+        form = ReservaForm(request.POST)
+        if form.is_valid():
+            reserva = form.save(commit=False)
+            reserva.sala = sala
+            reserva.usuario = request.user
+            reserva.save()
+            return redirect('valorar_experiencia')  # O a donde quieras redirigir después de la reserva
+    else:
+        form = ReservaForm(initial={'fecha': fecha_actual})
+
+    return render(request, 'rooms/reserva_sala.html', {
+        'form': form,
+        'sala': sala,
+        'fecha_actual': fecha_actual,
+        'fecha_limite': fecha_limite,
+    })
